3121897219ad9718358c88d8ae13e30a26b9ebd0
Ikken9 <pierosaucedo3@gmail.com>
2023-08-31 22:51:31 -0300

    Update Batt lib

--- lib/BatteryInterface/src/BatteryInterface.h
+++ lib/BatteryInterface/src/BatteryInterface.h
@@ -2,13 +2,26 @@
 // Created by Ikken on 10/8/2023.
 //
 
-#ifndef ESP_TOOLS_BATTERYINTERFACE_H
-#define ESP_TOOLS_BATTERYINTERFACE_H
+#ifndef CHEETAH_BATTERYINTERFACE_H
+#define CHEETAH_BATTERYINTERFACE_H
 
+#include <cstring>
+#include <Wire.h>
+
+#define IP5306I2C_ADDR      0x75
+#define SDA                 42
+#define SCL                 41
 
 class BatteryInterface {
+public:
+    BatteryInterface();
 
+    static uint8_t read(uint8_t reg_addr);
+    static int8_t getBatteryCharge();
+
+private:
+    static void write(uint8_t reg_addr, uint8_t data);
 };
 
 
-#endif //ESP_TOOLS_BATTERYINTERFACE_H
+#endif //CHEETAH_BATTERYINTERFACE_H
