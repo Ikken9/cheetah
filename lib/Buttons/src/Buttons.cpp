982744b26f2ae63858886f90ab17594fde8352ff
Ikken9 <pierosaucedo3@gmail.com>
2023-08-31 22:48:02 -0300

    Add buttons lib

---
+++ lib/Buttons/src/Buttons.cpp
@@ -0,0 +1,31 @@
+//
+// Created by Ikken on 27/8/2023.
+//
+
+#include "Buttons.h"
+
+Buttons::Buttons() {}
+
+void Buttons::up() {
+
+}
+
+void Buttons::left() {
+
+}
+
+void Buttons::down() {
+
+}
+
+void Buttons::right() {
+
+}
+
+void Buttons::ok() {
+
+}
+
+void Buttons::back() {
+
+}
---
+++ lib/Buttons/src/Buttons.h
@@ -0,0 +1,22 @@
+//
+// Created by Ikken on 27/8/2023.
+//
+
+#ifndef CHEETAH_BUTTONS_H
+#define CHEETAH_BUTTONS_H
+
+
+class Buttons {
+public:
+    Buttons();
+
+    void left();
+    void up();
+    void down();
+    void right();
+    void ok();
+    void back();
+};
+
+
+#endif //CHEETAH_BUTTONS_H
